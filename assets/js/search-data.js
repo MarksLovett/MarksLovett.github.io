c7acb7087b9f99b961141593cefb326aaeb76fdc
MarksLovett <89951396+MarksLovett@users.noreply.github.com>
2025-09-29 21:54:31 +0000

    Deploying to gh-pages from @ MarksLovett/MarksLovett.github.io@6016f350b468b6c98880ae181a750be604516403 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -76,6 +76,11 @@ ninja.data = [{
           description: "A Python package developed for my thesis to study &quot;influencer games&quot; using multi-agent reinforcement learning (MARL) and adaptive dynamics. The package enables users to run experiments with custom games and reproduce results from the paper.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
+            },},{id: "teaching-evolutionary-game-theory-math-30",
+          title: 'Evolutionary Game theory Math 30',
+          description: "A mathematical course on evolutionary game theory. The course introduces students to game theory and the concept of Nash equilibria, before introducing Evolutionary game theory to students. The topics covered are Evolutionary stable states, replicator dynamics, adaptive dynamics, asymmetric games, and evolutionary games on graphs. As a project based class the students are reponsibe for working with a team to dilver research project integrating evolutionary game theory into a problem of thier choice.",
+          section: "Teaching",handler: () => {
+              window.location.href = "/teaching/1_course/";
             },},{
         id: 'social-email',
         title: 'email',
